6ee48e6dd8c685b6772a9f9b34b86e9e64d71093
Zain Ul Abidin <zainulabidin9500@gmail.com>
2026-06-20 10:34:24 +0500

    add chapter3.py for gross pay calculation and score grading

---
+++ chpater3.py
@@ -0,0 +1,41 @@
+# 3.1 Write a program to prompt the user for hours and rate per hour using input to compute gross pay. Pay the hourly rate for the hours up to 40 and 1.5 times the hourly rate for all hours worked above 40 hours. Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75). You should use input to read a string and float() to convert the string to a number. Do not worry about error checking the user input - assume the user types numbers properly.
+
+hrs = input("Enter Hours:")
+h = float(hrs)
+rate = input("Enter rate:")
+r = float(rate)
+if h <= 40:
+    print(r*h)
+else:
+    normal = 40*r
+    over_work = (h - 40.0)*r*1.50
+    print(normal + over_work)
+
+
+'''
+3.3 Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error. If the score is between 0.0 and 1.0, print a grade using the following table:
+Score Grade
+>= 0.9 A
+>= 0.8 B
+>= 0.7 C
+>= 0.6 D
+< 0.6 F
+If the user enters a value out of range, print a suitable error message and exit. For the test, enter a score of 0.85.
+'''
+
+score = input("Enter Score: ")
+try:
+    s = float(score)
+    if s>=0 and s<=1:
+        if s >= 0.9:
+            print("A")
+        elif s >= 0.8:
+            print("B")
+        elif s >= 0.7:
+            print("C")
+        elif s >= 0.6:
+            print("D")
+        else:
+            print("F")
+except:
+    print("Out of range")
