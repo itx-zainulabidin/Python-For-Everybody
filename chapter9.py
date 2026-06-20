d5003c332116efbc953e2d671da7267eea60921c
Zain Ul Abidin <zainulabidin9500@gmail.com>
2026-06-20 20:44:41 +0500

    add chapter9.py for analyzing email senders and counting messages

---
+++ chapter9.py
@@ -0,0 +1,21 @@
+# 9.4 Write a program to read through the mbox-short.txt and figure out who has sent the greatest number of mail messages. The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file. After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
+
+
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+users = {}
+for line in handle:
+    if line.startswith("From "):
+        lst = line.split()
+        users[lst[1]] = users.get(lst[1],0) + 1
+        
+        
+maxkey,maxVal = None, -1    
+for key,value in users.items():
+    if value > maxVal:
+        maxVal = value
+        maxkey = key
+        
+print(maxkey,maxVal)
