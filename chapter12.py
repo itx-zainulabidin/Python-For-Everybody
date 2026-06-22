e141ced828f92f0cd7ffe882568e215c052a106e
Zain Ul Abidin <zainulabidin9500@gmail.com>
2026-06-22 13:27:17 +0500

    Add socket programming examples and HTML parsing with BeautifulSoup in chapter12.py

--- chapter12.py
+++ chapter12.py
@@ -1,3 +1,4 @@
+'''
 import socket
 
 mysock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -12,3 +13,67 @@ while True:
     print(data.decode())
 
 mysock.close()
+'''
+
+'''
+import urllib.request, urllib.parse, urllib.error
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+
+for line in fhand:
+    print(line.decode().strip())
+
+'''
+
+#####################
+'''
+import ssl
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+
+soup = BeautifulSoup(html,"html.parser")
+total = 0
+tags = soup('span')
+
+for tag in tags:
+    total += int(tag.contents[0])
+
+
+print("Total:", total)
+
+'''
+
+################### 
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl # defaults to certificate verification and most secure protocol (now TLS)
+
+# Ignore SSL/TLS certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+count = int(input("Enter count: "))
+pos = int(input("Enter position: "))
+print("Retrieving:",url)
+
+# Retrieve all of the anchor tags
+for i in range(count):
+    tags = soup('a')
+    url = tags[pos-1].get('href', None)
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup('a')
+
+    print("Retrieving:",url)
